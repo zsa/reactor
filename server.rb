464e7542575a1220734af3c5228fc7b2db625999
Tom de Grunt <tom@degrunt.nl>
2015-10-30 22:47:37 +0100

    Proof of concept working

--- server.rb
+++ server.rb
@@ -1,7 +1,41 @@
 require 'sinatra'
+require 'securerandom'
+require 'json'
+require 'liquid'
 
 configure { set :server, :puma }
 
 post '/' do
-	  "Hello World!"
+  uuid = SecureRandom.uuid()
+
+  tpl = File.read('./templates/ergodox_ez.liquid')
+  layout = JSON.load(request.body.read)
+  type = layout['type']
+
+  c_file = Liquid::Template.parse(tpl).render({'columns' => [7,7,6,7,5,2,1,3,7,7,6,7,5,2,1,3], 'layout' => layout})
+
+  # TODO: Commenting these out, for now - makes testing faster, but will wreak havoc
+  # Also - cp is very slow
+  # `cp -r qmk_firmware /tmp/#{uuid}`
+  #output_file = "/tmp/#{uuid}/keyboard/#{type}/keymaps/keymap_#{uuid}.c"
+
+  output_file = "qmk_firmware/keyboard/#{type}/keymaps/keymap_#{uuid}.c"
+
+  f = File.new(output_file, "w")
+  f.write(c_file)
+  f.close
+
+  puts "qmk_firmware/#{uuid}/keyboard/#{type}"
+
+  `cd qmk_firmware/keyboard/#{type} && make KEYMAP="#{uuid}"`
+
+  status = 200
+
+  begin
+    hex = File.read("qmk_firmware/keyboard/#{type}/#{type}.hex")
+  rescue Errno::ENOENT
+    status = 400
+  end
+
+  [status, hex]
 end
